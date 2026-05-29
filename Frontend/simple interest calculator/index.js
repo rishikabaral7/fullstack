58680727cad568fecefe81db58d98d11e2775b67
rishikabaral7 <rishikabaral007@gmail.com>
2026-05-29 07:40:03 +0545

    create simple interest calculator

---
+++ Frontend/simple interest calculator/index.js
@@ -0,0 +1,8 @@
+function simpleInterest(principal, time, rate){
+    si = (principal * time * rate)/100
+    console.log(`Simple Interest of p=${principal}, t=${time}, r=${rate} is`, si );
+    totalAmount = principal+ si;
+    console.log("Total Amount is", totalAmount)  
+
+}
+simpleInterest(300, 3, 4)
