b629fb44705dc5a8e49fdbbefba7926bf9105599
rishikabaral7 <rishikabaral007@gmail.com>
2026-05-29 07:22:07 +0545

    grade claculator

--- Frontend/Grade Calculator/index.js
+++ Frontend/Grade Calculator/index.js
@@ -13,12 +13,12 @@ console.log("Grade Percentage=", gradePercentage);
 
 if (gradePercentage >= 90 && gradePercentage <= 100) {
   console.log("Your grade is A");
-} else if (gradePercentage >= 80 && gradePercentage < 100) {
+} else if (gradePercentage >= 80 && gradePercentage < 90) {
   console.log("Your grade is B");
-} else if (gradePercentage >= 70 && gradePercentage < 100) {
+} else if (gradePercentage >= 70 && gradePercentage < 80) {
   console.log("Your grade is C");
-} else if (gradePercentage >= 60 && gradePercentage < 100) {
+} else if (gradePercentage >= 60 && gradePercentage < 70) {
   console.log("Your grade is D");
-} else if (gradePercentage >= 50 && gradePercentage < 100) {
+} else if (gradePercentage >= 50 && gradePercentage < 60) {
   console.log("Your grade is E");
 } else console.log("Failed");
