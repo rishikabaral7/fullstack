8adad5c981a8482883ad0486d1b2d45e8d9345cb
rishikabaral7 <rishikabaral007@gmail.com>
2026-06-10 07:34:07 +0545

    create result calculator

---
+++ Backend/week-1/resultCalculator.py
@@ -0,0 +1,24 @@
+total = 500   
+
+def result(math, science, social, nepali, english):
+    marks = math + science + social + nepali + english
+    totalPercentage = (marks/total)*100
+    return totalPercentage
+
+percentage =  int(result(30, 40, 50, 80, 40))
+print(f"Percentage: {percentage}%")
+
+if percentage > 90 and percentage <= 100:
+    print("Your grade is A+")
+
+elif percentage > 80 and percentage <= 90:
+    print("Your grade is A")
+
+elif percentage >70 and percentage <= 80:
+    print("Your grade is B+")
+
+elif percentage >60 and percentage <= 70:
+    print("Your grade is B")
+
+else:
+    print("You are Failed")
